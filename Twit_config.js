203ce3586f8c95aa97d209824d2f3d6bd778f5e8
MatrixChief <nigam.rishabh98@gamil.com>
2018-06-26 11:22:01 +0530

    twitter keys and config added. Server  can take tweets from twitter, based on many criteria like location, language etc

---
+++ Twit_config.js
@@ -0,0 +1,6 @@
+module.exports={
+    consumer_key:         'JgbVuomlVcvUje5g8ukJVAFnZ',
+    consumer_secret:      'Ev88viym69TTVCk2oyLOyEPT0FpAvKELzUsnRLPOTNAGnwBar1',
+    access_token:         '1011172840974925824-2tJCIdTcWwJUaTNCp3tZyxvD8D4Ukn',
+    access_token_secret:  'K9s7YHgIiztWl3cHzMx0VnfbY3lwtxnjtEcibAhhd2b4M',
+};
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-var express = require('express');
-var app = express();
-var port = 8080;
-var mongoose = require('mongoose');
-var MongoClient = require('mongodb').MongoClient;
-var twitterSchema = new mongoose.Schema({
-    username: String,
-    post: String
-});
-var User = mongoose.model('Twitter',twitterSchema);
+// var express = require('express');
+// var app = express();
+// var port = 8080;
+// var mongoose = require('mongoose');
+var Twit = require('twit');
+var Twit_config = require('./Twit_config');
+// var MongoClient = require('mongodb').MongoClient;
+// var twitterSchema = new mongoose.Schema({
+//     username: String,
+//     post: String
+// });
+var T = new Twit(Twit_config);
+// var User = mongoose.model('Twitter',twitterSchema);
+//
+// mongoose.Promise = global.Promise;
+// mongoose.connect("mongodb://localhost:27017/SocialMediaListen");
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/SocialMediaListen");
+console.log('Server is starting!');
+
+T.get('search/tweets', { q: 'banana since:2016-07-11', count: 2 }, function(err, data, response) {
+    if(err) throw err;
+    else{
+        console.log(data);
+    };
+});
