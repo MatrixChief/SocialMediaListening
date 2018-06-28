5a79d702bfaaf0715569e49316e50e9d13a1fc04
MatrixChief <nigam.rishabh98@gamil.com>
2018-06-28 12:54:28 +0530

    twitter part works and can show data

--- Public/myScript.js
+++ Public/myScript.js
@@ -1,9 +1,9 @@
 //send keyword to server for twitter search
 $(document).on('click','#submitBtnT',function(){
     $.ajax({
-        url: "http://localhost:8080/sendKeyword?key="+$('input[type=text][name=keyword]').val(),
-        type:"post",
-        data:JSON.stringify({key: $('input[type=text][name=keyword]').val()}),
+        url: "http://localhost:8080/sendKeywordT?key="+$('input[type=text][name=keyword]').val(),
+        type: "post",
+        data: JSON.stringify({key: $('input[type=text][name=keyword]').val()}),
         success: function(result){
             console.log(result);
             window.location.href='/chooseGetter';
@@ -11,25 +11,41 @@ $(document).on('click','#submitBtnT',function(){
     });
 })
 
-//send keyword to server for facebook search
-$(document).on('click','#submitBtnF',function(){
-    $.ajax({
-        url: "http://localhost:8080/sendKeyword?key="+$('input[type=text][name=keyword]').val(),
-        type:"post",
-        data:JSON.stringify({key: $('input[type=text][name=keyword]').val()}),
-        success: function(result){
-            console.log(result);
-            window.location.href='/chooseGetter';
-        }
-    });
-})
+// //send keyword to server for facebook search
+// $(document).on('click','#submitBtnF',function(){
+//     $.ajax({
+//         url: "http://localhost:8080/sendKeywordF?key="+$('input[type=text][name=keyword]').val(),
+//         type:"post",
+//         data:JSON.stringify({key: $('input[type=text][name=keyword]').val()}),
+//         success: function(result){
+//             console.log(result);
+//             window.location.href='/chooseGetter';
+//         }
+//     });
+// })
 
 //send choice for search
 $(document).on('click','#sendSelect',function(){
-    if($('#select').val()=="Twitter"){
+    if($('#select_1').val()=="Twitter"){
         window.location.href='/twitterSearch';
     }
     else{
         window.location.href='/facebookSearch';
     }
+})
+
+$(document).on('click','#sendGetter',function(){
+    $.ajax({
+        url: "http://localhost:8080/sendGetter?key="+$('#select').val(),
+        type: "post",
+        data: JSON.stringify({key: $('#select').val()}),
+        success: function(resultArray){
+            var html = '<tr><th>Id</th><th>Post</th></tr>';
+            for(var i = 0; i < resultArray.length; i++){
+                html+='<tr><td>'+resultArray[i]['user_id']+"</td><td>"+resultArray[i]['post']+"</td></tr>";
+            }
+            console.log(html);
+            $('#posts').html(html);
+        }
+    })
 })
--- server.js
+++ server.js
@@ -8,42 +8,64 @@ var Twit_config = require('./Config/Twit_config');
 var T = new Twit(Twit_config);
 
 var twitterSchema = new mongoose.Schema({
-    username: String,
+    user_id: String,
     post: String
 });
-var User = mongoose.model('Twitter',twitterSchema);
+// var facebookSchema = new mongoose.Schema({
+//     user_id: String,
+//     post: String
+// });
+var TUser= mongoose.model('Twitter', twitterSchema);
+// var FUser = mongoose.model('Facebook', facebookSchema);
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/SocialMediaListen");
 
 app.use("/Public",express.static(__dirname+"/Public"));
+////////////////////////////////////////////////////////////////////////////////
 
-app.post("/sendKeyword", (req, res) => {
-    var keyword = req.query.key; // keyword for search
+app.post("/sendKeywordT", (req, res) => {
+    var keyword = req.query.key;
     var params = {
         q:keyword,
         lang:'en',
-        count: 3
-    }//params for search
+        count: 100
+    }
+    var newUser = new Array();
+    var newPost = new Array();
 
     T.get('search/tweets', params, function(err, data, response) {
         if(err) throw err;
         else{
-            console.log(data);
+            // console.log(data);
             for(var i in data.statuses){
-                console.log("this is metadata: "+JSON.stringify(data.statuses[i].metadata));
+                newUser.push(data.statuses[i].id_str);
+                newPost.push(data.statuses[i].text);
             }
+            for(var i=0; i<newUser.length; i++){
+                var myData=new TUser({user_id: newUser[i], post: newPost[i]});
+                myData.save()
+                    .then(item => {
+                        res.send("item saved to database");
+                    });
+            };
         };
     });
-    res.send("Items saved to database!")
-        // .then(item => {
-        //     res.send("item saved to database");
-        // })
-        // .catch(err => {
-        //     res.status(400).send("unable to save to database");
-        // });
 });
 
+app.post("/sendGetter",(req,res)=>{
+    var platform = req.query.key;
+    if(platform=="Twitter"){
+        TUser.find({}, {_id: 0,user_id: 1, post: 1}, function(err, result){
+            if(err) throw err;
+            res.send(result);
+        })
+    }
+    // else{
+    //
+    // }
+})
+////////////////////////////////////////////////////////////////////////////////
 app.get("/twitterSearch", (req, res) => {
     res.sendFile(__dirname + "/View/twitterSearch.html");
 });
